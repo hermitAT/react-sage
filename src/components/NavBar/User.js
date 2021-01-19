b19e046beb04bddeb64f7d19142bbbe6e442c9cb
hermitAT <ahthornev2@gmail.com>
2021-01-18 23:25:57 -0800

    Edit scss for navbar and main index.js file

--- src/components/NavBar/User.js
+++ src/components/NavBar/User.js
@@ -10,11 +10,11 @@ export default function User(props) {
         <span className="user__card--name text--semi-bold">{props.user.name}</span>
         <img className="user__card--avatar" src={props.user.avatar} alt="User Avatar"/>
       </section>
-      <section className="user__card--icon">
-        <FontAwesomeIcon icon="thumbs-up" size="lg" />
-        <FontAwesomeIcon icon="bookmark" size="lg" />
-        <FontAwesomeIcon icon="user" size="lg" />
-        <FontAwesomeIcon icon="sign-out-alt" size="lg" onClick={props.onLogout} />
+      <section className="user__card--links">
+        <FontAwesomeIcon className="user__card--icon" icon="thumbs-up" size="lg" />
+        <FontAwesomeIcon className="user__card--icon" icon="bookmark" size="lg" />
+        <FontAwesomeIcon className="user__card--icon" icon="user" size="lg" />
+        <FontAwesomeIcon className="user__card--icon" icon="sign-out-alt" size="lg" onClick={props.onLogout} />
       </section>
     </main>
   );
