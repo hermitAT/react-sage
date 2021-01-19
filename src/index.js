f2461b07d6200a0809c16172a0ec0cc1ae131f97
hermitAT <ahthornev2@gmail.com>
2021-01-18 16:39:07 -0800

    Create initial component, intialize storybook

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import './index.scss';
+
+import App from 'components/App';
 
 ReactDOM.render(
   <React.StrictMode>
