29253acd3b5beb93cb0e1a8d1b2fea2095f9bda1
hermitAT <ahthornev2@gmail.com>
2021-01-20 23:20:09 -0800

    Add logic to show ingredients on form, refactor JSX from recipe page

--- src/components/Create/Show.js
+++ src/components/Create/Show.js
@@ -1,15 +1,27 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+import IngredientList from "../IngredientsList";
+
 import "./index.scss";
 
 export default function Show(props) {
+
   return (
     <main>
+      {props.ingredients && (
+        <IngredientList ingredients={props.ingredients} />
+      )}
+      {!props.ingredients && (
+        <div>
+          <h4>...</h4>
+          <h4>Oh, looks like you don't have any ingredients!</h4>
+          <h4>Hit that button below to get started!</h4>
+        </div>
+      )}
       <button type="button" onClick={props.add}>
         <FontAwesomeIcon icon="plus" size="lg" /> Add Ingredient
       </button>
-      <h4>placeholder</h4>
     </main>
   );
 };
