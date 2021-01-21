88ca5f545bc2e010f8aafa8e98f8aa24ca2aa714
ngampit <ngampitt@yahoo.com>
2021-01-21 13:13:38 -0800

    add user page

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -5,4 +5,3 @@ import Button from "components/Button";
 export default {title: "button"};
 export const primary = () => <Button confirm>confirm</Button>
 export const secondary = () => <Button cancel>cancel</Button>
-
---
+++ src/stories/User.stories.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+import User from "components/User";
+
+export default {title: "user"};
+export const primary = () => <User/>
