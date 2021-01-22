a708008ad310990630b704678a2eb56a37c9cd72
hermitAT <ahthornev2@gmail.com>
2021-01-22 01:11:30 -0800

    Add jumbotron header for home page, continue to edit style

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,40 +1,62 @@
-import React from 'react';
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import './index.scss';
+import "./index.scss";
 
-import RecipeCard from '../RecipeCard';
-import useHomeData from 'hooks/useHomeData';
-
+import straw from "./straw.gif";
+import RecipeCard from "../RecipeCard";
+import useHomeData from "hooks/useHomeData";
 
 export default function Home(props) {
-
   const { state } = useHomeData();
 
   const { daily_cocktail, most_pop_alc, most_pop_non, flavours } = state;
 
   return (
     <main className="home__layout">
-      <section className="home__layout-upper">
-      <div className="home__layout-group">
-        <h2 className="home__headings"><FontAwesomeIcon icon="cocktail" size="lg" />Cocktail of the Day</h2>
-        {daily_cocktail.recipe && <RecipeCard recipe={daily_cocktail} flavours={flavours} />}
+      <div className="home__layout-header">
+        <h3>Welcome to CocktailSage, the app you turn to for cocktail guidance.</h3>
       </div>
-      <div className="home__layout-group">
-        <h2 className="home__headings"><FontAwesomeIcon icon="wine-glass-alt" size="lg" />Cocktail of the Day</h2>
-        {daily_cocktail.recipe && <RecipeCard recipe={daily_cocktail} flavours={flavours} />}
-      </div>
-      </section>
-      <section className="home__layout-upper">
-      <div className="home__layout-group">
-        <h2 className="home__headings"><FontAwesomeIcon icon="glass-cheers" size="lg" />Best Rated Alcoholic</h2>
-        {most_pop_alc.recipe && <RecipeCard recipe={most_pop_alc} flavours={flavours} />}
-      </div>
-      <div className="home__layout-group">
-        <h2 className="home__headings"><FontAwesomeIcon icon="lemon" size="lg" />Best Rated Non-Alcoholic</h2>
-        {most_pop_non.recipe && <RecipeCard recipe={most_pop_non} flavours={flavours} />}
-      </div>
-      </section>
+        <section className="home__layout-upper">
+          <div className="home__layout-group">
+            <h2 className="home__headings">
+              <FontAwesomeIcon icon="cocktail" size="lg" />
+              Cocktail of the Day
+            </h2>
+            {daily_cocktail.recipe && (
+              <RecipeCard recipe={daily_cocktail} flavours={flavours} />
+            )}
+          </div>
+          <div className="home__layout-group">
+            <h2 className="home__headings">
+              <FontAwesomeIcon icon="wine-glass-alt" size="lg" />
+              Cocktail of the Day
+            </h2>
+            {daily_cocktail.recipe && (
+              <RecipeCard recipe={daily_cocktail} flavours={flavours} />
+            )}
+          </div>
+        </section>
+        <section className="home__layout-upper">
+          <div className="home__layout-group">
+            <h2 className="home__headings">
+              <FontAwesomeIcon icon="glass-cheers" size="lg" />
+              Best Rated Alcoholic
+            </h2>
+            {most_pop_alc.recipe && (
+              <RecipeCard recipe={most_pop_alc} flavours={flavours} />
+            )}
+          </div>
+          <div className="home__layout-group">
+            <h2 className="home__headings">
+              <FontAwesomeIcon icon="lemon" size="lg" />
+              Best Rated Non-Alcoholic
+            </h2>
+            {most_pop_non.recipe && (
+              <RecipeCard recipe={most_pop_non} flavours={flavours} />
+            )}
+          </div>
+        </section>
     </main>
-  )
-};
+  );
+}
--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 
-
 import logo from "./logo.svg";
 import Dropdown from "./Dropdown";
 import User from "./User";
