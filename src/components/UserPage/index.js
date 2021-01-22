76e2627d44dce111795978532066de6c83f814e9
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2021-01-21 22:12:59 -0800

    Complete User Page without fetching data

--- src/components/UserPage/index.js
+++ src/components/UserPage/index.js
@@ -1,31 +1,39 @@
-import { render } from "@testing-library/react";
 import React from "react";
 import "./UserPage.scss";
+import Button from "components/Button"
 const { timeAgo } = require("helpers/timeAgo")
 
 export default function User(props) {
 
   const { name, user_avatar, created_at, recipes_id, favorites_id } = props;
 
-   return (
-     <div id="user-page">
-       <div id="title-page" class="text-container">
-         <h2>Welcome back, {name}</h2>
-       </div>
-       <div class="row-group">
-         <div id="image-description">
-           <img src={user_avatar} />
-           <article class='text-container'>
-             joined CocktailSage : {timeAgo(created_at)}  <br/>
-             recipes created : {recipes_id.length} <br/>
-             recipe favorite : {favorites_id.length}  <br/>
-           </article>
-         </div>
-         <div id="recipe-favorite">
-           <h3 class="text-container">My recipe</h3>
-           <h3 class="text-container">My favorite recipe</h3>
-         </div>
-       </div>
-     </div>
+  return (
+    <div id="user-page">
+      <div id="title-page" >
+        <h2 class="text-container">Welcome back, {name}!</h2>
+      </div>
+      <div class="row-group">
+        <div id="image-description">
+          <img src={user_avatar} />
+          <article class='text-container'>
+            <p>joined CocktailSage {timeAgo(created_at)}</p>
+            <p>created {recipes_id.length} recipe{recipes_id.length > 1 ? 's' : ''}</p>
+            <p>{favorites_id.length} recipes are favorite</p>
+          </article>
+        </div>
+        <div id="recipe-favorite">
+          <Button
+            confirm
+            onClick={console.log('MY')}>
+            My recipes
+          </Button>
+          <Button
+            confirm
+            onClick={console.log('MY')}>
+            Favorite recipes
+          </Button>
+        </div>
+      </div>
+    </div>
   );
-  }
+}
