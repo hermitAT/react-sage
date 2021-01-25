d64f4f4a6a4cd7642dcbb8fc02ec3606e31d3563
ngampit <ngampitt@yahoo.com>
2021-01-24 16:48:32 -0800

    refactor user page data fetch

--- src/components/UserPage/index.js
+++ src/components/UserPage/index.js
@@ -1,33 +1,46 @@
-import React from "react";
-import "./UserPage.scss";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+
+import RecipeList from "components/RecipeList";
 import Button from "components/Button";
-import  { timeAgo } from "helpers/timeAgo";
-import useUserData from "hooks/useUserData";
-import UserRecipeList from "./UserRecipeList";
+import { timeAgo } from "helpers/timeAgo";
+
+import "./UserPage.scss";
 
 export default function User(props) {
+  const [user, setUser] = useState("");
+  const [pages, setPages] = useState("");
 
-const { getUserRecipe, recipes } = useUserData();
+  let { id } = useParams();
+
+  useEffect(() => {
+    Promise.all([
+    axios.get(`/api/users/${id}`),
+    axios.get(`/api/recipes/search?user_id=${id}`)
+    ])
+      .then((all) => {
+        setUser(prev => all[0].data);
+        setPages(prev => all[1].data.result.relevance);
+        })
+        .catch((e) => console.error(e));
+    }, [id]);
  
   return (
     <>
-    {props.user &&  (
+    {user &&  (
       <div className="user__page">
         <div className="user__page-top">
-          <img className="user__page-avatar" src={props.user.user.user_avatar} alt="user avatar" />
+          <img className="user__page-avatar" src={user.user.user_avatar} alt="user avatar" />
           <div class="user__page-controls">
             <div className="user__page-title">
-              <h2 className="user__page-h2">Welcome back, {props.user.user.name}!</h2>
+              <h2 className="user__page-h2">Welcome back, {user.user.name}!</h2>
             </div>
             <div className="user__page-buttons">
-              <Button
-                className="user__page-button"
-                onClick={() => getUserRecipe(props.user.recipes_id)}>
+              <Button className="user__page-button" onClick={console.log("hi")}>
                 My Recipes
               </Button>
-              <Button
-                className="user__page-button"
-                onClick={() => getUserRecipe(props.user.favorites_id)}>
+              <Button className="user__page-button" onClick={console.log("hi")}>
                 My Favorite Recipes
               </Button>   
             </div>
@@ -35,14 +48,14 @@ const { getUserRecipe, recipes } = useUserData();
         </div>
         <div className="user__page-footer">
         <article className='user__page-details'>
-          <p>Joined CocktailSage {timeAgo(props.user.user.created_at)}.</p>
-          <p>Created {props.user.recipes_id.length} recipe{props.user.recipes_id.length > 1 ? 's' : ''}!</p>
-          <p>{props.user.favorites_id.length} favourite recipe{props.user.favorites_id.length > 1 ? 's' : ''}!</p>
+          <p>Joined CocktailSage {timeAgo(user.user.created_at)}.</p>
+          <p>Created {user.recipes_id.length} recipe{user.recipes_id.length > 1 ? 's' : ''}!</p>
+          <p>{user.favorites_id.length} favourite recipe{user.favorites_id.length > 1 ? 's' : ''}!</p>
         </article>
-        { recipes && (
-          <UserRecipeList recipes={recipes} />
-        )}
         </div>
+        {pages && (
+          <RecipeList pages={pages} />
+        )}
       </div>
     )}
     </>       
